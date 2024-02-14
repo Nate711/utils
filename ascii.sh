9e9ee52c1cefece8a15e9eb9b002b7fe01e59dc6
Nathan Kau <nathankau@gmail.com>
2024-02-14 08:37:23 -0800

    add other faces

--- ascii.sh
+++ ascii.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Check if a file path argument is provided
+if [ "$#" -eq 0 ]; then
+    text_file="banner_regular_eyes.txt" # Default file path
+else
+    text_file="$1" # Use the provided argument
+fi
 
+
+# Assign the first command line argument to a variable
+# text_file="$1"
 
 # Iterate over each logged-in session
 while IFS= read -r session; do
@@ -13,9 +22,8 @@ while IFS= read -r session; do
         # Send each line of your text file to the user's TTY
         while IFS= read -r line; do
             echo -e "$line" > "/dev/$tty"
-        done < /home/pi/utils/banner.txt
+        done < /home/pi/utils/"$text_file"
     else
         echo "TTY '/dev/$tty' not found for user $user."
     fi
 done <<< "$(who)"
-
