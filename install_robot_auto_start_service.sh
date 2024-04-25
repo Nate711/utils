21999675cf0b0a5aa3cb102f5c8c798cf3188914
Gabrael Levine <gabrael@stanford.edu>
2024-04-25 08:34:47 -0700

    update script and add tty1 permission schange service

--- install_robot_auto_start_service.sh
+++ install_robot_auto_start_service.sh
@@ -1,3 +1,5 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 sudo ln -s ${SCRIPT_DIR}/robot.service /etc/systemd/system/robot.service
+sudo ln -s ${SCRIPT_DIR}/change_tty1_permissions.service /etc/systemd/system/change_tty1_permissions.service
+sudo systemctl enable change_tty1_permissions
 sudo systemctl enable robot
