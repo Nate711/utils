98db3168c26d720be73172b124f14ccc1ad2375c
Gabrael Levine <gabrael@stanford.edu>
2024-05-18 14:38:59 -0700

    Show battery percentage

--- check_batt_voltage.py
+++ check_batt_voltage.py
@@ -52,18 +52,30 @@ def main():
     )
     args = parser.parse_args()
     i2c = busio.I2C(board.SCL, board.SDA)
-    THRESHOLD = 3.5 * 5
+    NUM_CELLS = 5
+    MIN_VOLTAGE = 3.5 * NUM_CELLS
+    MAX_VOLTAGE = 4.2 * NUM_CELLS
+    CELL_VOLTAGES = [4.2, 4.15, 4.11, 4.08, 4.02, 3.98, 3.95, 3.91, 3.87, 3.85, 3.84, 3.82, 3.8, 3.79, 3.77, 3.75, 3.73, 3.71, 3.69, 3.61, 3.27, 3.27]
+    CELL_PERCENTAGES = [100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0, 0]
+
+    bat_voltage = get_battery_voltage(i2c)
+    cell_voltage = max(min(bat_voltage / NUM_CELLS, CELL_VOLTAGES[0]), CELL_VOLTAGES[-1])
+    cell_percentage = 100
+    for i in range(len(CELL_VOLTAGES) - 1):
+        if CELL_VOLTAGES[i + 1] < cell_voltage < CELL_VOLTAGES[i]:
+            proportion = (cell_voltage - CELL_VOLTAGES[i + 1]) / (CELL_VOLTAGES[i] - CELL_VOLTAGES[i + 1] + 1e-6)
+            cell_percentage = proportion * CELL_PERCENTAGES[i] + (1 - proportion) * CELL_PERCENTAGES[i + 1]
+            break
 
     if args.service_mode:
         while True:
-            bat_voltage = get_battery_voltage(i2c)
-            if bat_voltage < THRESHOLD:
+            if bat_voltage < MIN_VOLTAGE:
                 broadcast_message(
-                    f"\033[91mWARNING: Battery voltage {bat_voltage:0.2f} is below threshold {THRESHOLD}!\033[0m"
+                    f"\033[91mWARNING: Battery voltage {bat_voltage:0.2f} is below threshold {MIN_VOLTAGE}!\033[0m"
                 )
             time.sleep(10)  # Check every 10 seconds
     else:
-        print(f"Battery voltage: {get_battery_voltage(i2c):0.2f}V")
+        print(f"Battery:\t{int(cell_percentage)}%\t{bat_voltage:0.2f}V")
 
 
 if __name__ == "__main__":
