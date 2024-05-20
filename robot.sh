2a078f0f6df4c60a101416c96149960a26ab3438
Gabrael Levine <gabrael@stanford.edu>
2024-05-20 13:07:12 -0700

    Revert CPU governor change (not working)

--- robot.sh
+++ robot.sh
@@ -2,7 +2,6 @@
 
 source /opt/ros/humble/setup.bash
 source /home/pi/ros2_ws/install/setup.bash
-echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 ros2 launch ~/ros2_ws/src/control_board_hardware_interface/test/test_state_publisher.launch.py &
 python3 /home/pi/StanfordQuadruped/examples/pupper_v3_ds4_control.py & 
 ros2 run pupper_feelings face_control
