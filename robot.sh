b3d9abed275398297be1670f15175ce3bf6b672f
Gabrael Levine <gabrael@stanford.edu>
2024-05-25 12:48:09 -0700

    Update robot.sh

--- robot.sh
+++ robot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source /opt/ros/humble/setup.bash
+source /opt/ros/jazzy/setup.bash
 source /home/pi/ros2_ws/install/setup.bash
-ros2 launch ~/ros2_ws/src/control_board_hardware_interface/test/test_state_publisher.launch.py &
-python3 /home/pi/StanfordQuadruped/examples/pupper_v3_ds4_control.py & 
+ros2 launch neural_controller launch.py &
 ros2 run pupper_feelings face_control
